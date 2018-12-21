b87dee1995c6aeec68297e57bcac4c86192d99d1
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2018-12-21 14:42:27 +0800

    add cuda support

--- rnn.py
+++ rnn.py
@@ -3,7 +3,11 @@ from torch import nn
 from main import*
 import torch.utils.data as Data
 
-N = 5
+
+device = 'cpu'
+#device = 'cuda'
+
+N = 48
 
 def getTrainData():
     train_X, train_Y = read_TrainData('train.csv', N=1)
@@ -40,6 +44,7 @@ try:
     model = torch.load('net.pkl')
 except:
     model = RNN()
+model.to(device)
 
 
 loss_func = torch.nn.MSELoss()
@@ -51,9 +56,10 @@ trainloss = []
 testloss = []
 for epoch_ in range(epoch):
     for step, (b_x, b_y) in enumerate(loader):       
-        b_x = torch.unsqueeze(b_x,dim = 1)
+        b_x = torch.unsqueeze(b_x,dim = 1).to(device)
         output = model(b_x)
         output = torch.squeeze(output,dim=1)
+        b_y = b_y.to(device)
         loss = loss_func(output, b_y)
         optimizer.zero_grad()                           
         loss.backward()                                 
@@ -61,13 +67,13 @@ for epoch_ in range(epoch):
 
 
         if step % 10000 == 0:
-            test_output = model(torch.unsqueeze(test_X,dim=1))   
+            test_output = model(torch.unsqueeze(test_X,dim=1).to(device))   
             test_output = torch.squeeze(test_output,dim=1) 
                  # (time_step(N), 1 , input_size)
-            test_loss = np.mean(((test_output.detach().numpy()-test_Y.detach().numpy())**2))
+            test_loss = np.mean(((test_output.cpu().detach().numpy()-test_Y.detach().numpy())**2))
             trainloss.append(loss.data.numpy())
             testloss.append(test_loss)
-            print('Epoch: ', epoch_, '| train loss: %.4f' % loss.data.numpy(), '| test loss: %.4f' % test_loss)
+            print('Epoch: ', epoch_, '| train loss: %.4f' % loss.data.cpu().numpy(), '| test loss: %.4f' % test_loss)
             torch.save(model, 'net.pkl')
 plotting(trainloss,testloss,'./rnn.png')
 
